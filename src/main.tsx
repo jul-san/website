90f37a1c34c634b7a0b93142dc3d27973845f5f5
Julian Sanchez <julianwsan@gmail.com>
2023-08-14 16:52:45 -0400

    imported chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'; // Import your custom theme
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App/>
+    </ChakraProvider>
   </React.StrictMode>,
 )
