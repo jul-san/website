32f4af4459fa271f44f1a5521f2f4223a94758c6
Julian Sanchez <julianwsan@gmail.com>
2023-08-30 16:56:05 -0400

    Modified main.tsx to include app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import Message from './Message';
+import ResumePage from './pages/ResumePage/ResumePage';
+import BlogHome from './pages/BlogPage/BlogHome';
+import HomePage from './pages/HomePage/HomePage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {path: "/", element: <HomePage/>,},
+  {path: "/resume", element: <ResumePage/>,},
+  {path: "/blog", element: <BlogHome/>,},
+]);
 
 function App(){
-  return <div><Message/></div>;
+  return(
+    <>
+      <RouterProvider router={router} />
+    </>
+    
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
-import theme from './theme'; // Import your custom theme
-
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
