5fae2400e0a516dc20766985018f78a715f487c5
Julian Sanchez <julianwsan@gmail.com>
2023-09-06 22:22:02 -0400

    Fixed typos and added experience

--- src/pages/ResumePage/ResumePage.tsx
+++ src/pages/ResumePage/ResumePage.tsx
@@ -22,10 +22,11 @@ function ResumePage(){
                 <Heading fontFamily='Roboto Mono' paddingTop='30px'>Experience</Heading>
                 <Section name="Northrop Grumman" position='Software Engineer Intern' date='May 2023 - July 2023' img='src/img/northrop.png'/>
                 <Section name="Code Ninjas" position='Code Sensei' date='May 2022 - August 2022' img='src/img/cnt.png'/>
-                <Section name="FIRST Robotics" position='Programmer' date='Auguest 2018 - May 2022' img='src/img/frc.png'/>
+                <Section name="FIRST Robotics" position='Programmer' date='August 2018 - May 2022' img='src/img/frc.png'/>
 
                 <Heading fontFamily='Roboto Mono' paddingTop='30px'>Involvement</Heading>
                 <Section name="Society of Asian Scientists and Engineers" position='Vice President-Internal' date='May 2023 - Present' img='src/img/sasefull.png'/>
+                <Section name="eSports at FSU" position='Gold Team - Overwatch 2' date='September 2023 - Present' img='src/img/esports2.png'/>
                 <Section name="Chess Club" position='Member' date='November 2022 - Present' img='src/img/chesswt.png'/>
 
             </Container>
