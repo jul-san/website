4fc8dde16166a0cd7a9ea0fa45f6ebb210820373
Julian Sanchez <julianwsan@gmail.com>
2023-12-02 15:39:28 -0500

    updated the body + changed font

--- src/pages/HomePage/TextBody.tsx
+++ src/pages/HomePage/TextBody.tsx
@@ -4,20 +4,20 @@ function TextBody(){
     return(
         <>
         <Container paddingTop='10px'>
-            <Text fontFamily='Roboto Mono' fontSize='14px'>                
-                I'm currently studying CS at Florida State University, where most of my days 
-                are spent learning about various programming concepts through personal projects 
-                or prepping for interviews.<br/><br/>
+            <Text fontWeight='350' fontSize='16px'>                
+                Hi, I'm Julian, a sophomore at Florida State University who loves to learn about
+                anything and everything.<br/><br/>
 
-                In my spare time, I enjoy playing rapid chess where I am a member of FSU's
-                chess club, and also regularly play online. I also enjoy playing 
-                video games such as Overwatch, Fallout, and Stardew Valley. <br/><br/>
+                <b>Previously:</b> Experienced what working in defense is like.<br/>
+                <b>Currently:</b> Building with AI to see how it can make my life easier.<br/>
+                <b>Future:</b> Explore the intersection between software and systems engineering.<br/><br/>
+
 
                 Filipino American, second-year student, South Floridian. <br/><br/>
             </Text>
 
 
-            <Text fontFamily='Roboto Mono'>
+            <Text>
                 <Link href='https://www.linkedin.com/in/julianwsanchez/' textDecoration='underline'>LinkedIn</Link>
                 •
                 <Link href='https://github.com/jul-san' textDecoration='underline'>GitHub</Link> 
