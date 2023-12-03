817500c0da15125b3a046d9485489e8f2baecf24
Julian Sanchez <julianwsan@gmail.com>
2023-12-03 15:12:59 -0500

    updated resume section

--- src/pages/ResumePage/ResumePage.tsx
+++ src/pages/ResumePage/ResumePage.tsx
@@ -8,7 +8,7 @@ function ResumePage(){
         <>
             <Navbar/>
 
-            <Container fontFamily='Roboto Mono'>
+            <Container>
 
             <a  href={PDF}
                 style={{ 
@@ -16,18 +16,19 @@ function ResumePage(){
                     textDecoration: 'underline'
                     }}>View Full PDF Here</a>
 
-                <Heading fontFamily='Roboto Mono' paddingTop='10px'>Education</Heading>
+                <Heading paddingTop='10px'>Education</Heading>
                 <Section name="Florida State University" position='B.S. in Computer Science' date='Expected Grad. May 2026' img='src/img/fsu.png'/>
                 
-                <Heading fontFamily='Roboto Mono' paddingTop='30px'>Experience</Heading>
+                <Heading paddingTop='30px'>Experience</Heading>
+                <Section name="Massachusetts Institute of Technology" position="Summer Research Intern" date='May 2024 - August 2024' img='src/img/mit.png'></Section>
                 <Section name="Northrop Grumman" position='Software Engineer Intern' date='May 2023 - July 2023' img='src/img/northrop.png'/>
                 <Section name="Code Ninjas" position='Code Sensei' date='May 2022 - August 2022' img='src/img/cnt.png'/>
                 <Section name="FIRST Robotics" position='Programmer' date='August 2018 - May 2022' img='src/img/frc.png'/>
 
-                <Heading fontFamily='Roboto Mono' paddingTop='30px'>Involvement</Heading>
+                <Heading paddingTop='30px'>Involvement</Heading>
                 <Section name="Society of Asian Scientists and Engineers" position='Vice President-Internal' date='May 2023 - Present' img='src/img/sasefull.png'/>
-                <Section name="eSports at FSU" position='Gold Team - Overwatch 2' date='September 2023 - Present' img='src/img/esports2.png'/>
                 <Section name="Chess Club" position='Member' date='November 2022 - Present' img='src/img/chesswt.png'/>
+                <Section name="eSports at FSU" position='Gold Team - Overwatch 2' date='September 2023 - November 2023' img='src/img/esports2.png'/>
 
             </Container>
 
--- src/pages/ResumePage/Section.tsx
+++ src/pages/ResumePage/Section.tsx
@@ -1,4 +1,4 @@
-import { VStack, Flex, Image, Text } from "@chakra-ui/react";
+import { HStack, VStack, Flex, Image, Text, Box} from "@chakra-ui/react";
 
 interface CardInfo{
     name: string;
@@ -10,19 +10,41 @@ interface CardInfo{
 function Section({name, position, date, img}: CardInfo){
     return(  
         <>
-            <Flex direction={{ base: 'column', md: 'row' }} align="center">
-                <Image alt="fsu logo" 
+            <Flex 
+                direction={{ base: 'column', md: 'row' }} 
+                align="center" 
+                justifyContent="center"
+                gap={{ base: '0px', md: '20px' }} // Add a gap between the boxes
+                >
+                    <Box
+                        display="flex"
+                        alignItems="center" 
+                        justifyContent="center"
+                        boxSize="250px"
+                    >
+                        <Image 
+                        alt="fsu logo"
                         src={img}
-                        boxSize={{ base: '250px', md: '250px' }}
-                        objectFit='scale-down'
-                        alignContent='left'
+                        boxSize="full" // Use 'full' to make the image take the full size of the box
+                        objectFit="scale-down"
                         />
-                <VStack wrap='wrap' paddingLeft='10px'>
-                    <Text align='center'>{name}</Text>
-                    <Text align='center'>{position}</Text>
-                    <Text align='center'>{date}</Text>
-                </VStack>
+                    </Box>
+
+                    <Box 
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        boxSize="270px" // Set equal size for both boxes
+                        fontSize="18px"
+                    >
+                        <VStack wrap="wrap" paddingLeft="10px" textAlign="center">
+                            <Text>{name}</Text>
+                            <Text>{position}</Text>
+                            <Text>{date}</Text>
+                        </VStack>
+                    </Box>
             </Flex>
+     
         </>
     )
 }
